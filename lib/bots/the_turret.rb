116506f578839ab5487602e2a5debd2a9b71575c
David Workman <david.workman@carwow.co.uk>
2026-04-29 14:03:44 +0100

    Only shoot if enemy is seen

--- lib/bots/the_turret.rb
+++ lib/bots/the_turret.rb
@@ -16,10 +16,10 @@ class TheTurret < BattleBots::Bots::Bot
 
   def think
     enemy = select_target
-    if enemy
-      bearing, distance = calculate_vector_to(enemy)
-      aim_turret(bearing, distance)
-    end
+    return unless enemy
+
+    bearing, distance = calculate_vector_to(enemy)
+    aim_turret(bearing, distance)
     @shoot = true
   end
 end
